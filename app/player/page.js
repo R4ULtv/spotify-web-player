5697d6a30c8d64188868b10a2275e6c25e52359f
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-04 16:16:39 +0200

    add player

---
+++ app/player/page.js
@@ -0,0 +1,19 @@
+import { SpotifyProvider } from "@/components/providers/SpotifyProvider";
+import RandomCircle from "@/components/ui/background";
+import SpotifyPlayer from "@/components/ui/SpotifyPlayer";
+import Navigation from "@/components/ui/Navigation";
+import { auth } from "@/lib/auth";
+
+export default async function UserAvatar() {
+  const session = await auth();
+
+  return (
+    <SpotifyProvider session={session}>
+      <main className="relative w-full h-screen z-10">
+        <Navigation session={session} />
+        <SpotifyPlayer />
+      </main>
+      <RandomCircle />
+    </SpotifyProvider>
+  );
+}
