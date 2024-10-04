031159cbe2bafc89f4c84e2f2ada75262d8fae65
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-04 16:16:45 +0200

    add provider

---
+++ components/providers/SpotifyProvider.js
@@ -0,0 +1,166 @@
+"use client";
+import React, {
+  createContext,
+  useState,
+  useEffect,
+  useContext,
+  useCallback,
+  useMemo,
+} from "react";
+import { extractColors } from "extract-colors";
+
+const SpotifyContext = createContext();
+
+export const SpotifyProvider = ({ children, session }) => {
+  const [currentTrack, setCurrentTrack] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [currentPalette, setCurrentPalette] = useState(null);
+
+  const togglePlay = useCallback(async () => {
+    if (!currentTrack) return;
+    try {
+      const res = await fetch(
+        `https://api.spotify.com/v1/me/player/${isPlaying ? "pause" : "play"}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${session?.accessToken}`,
+          },
+        }
+      );
+      setIsPlaying(!isPlaying);
+    } catch (error) {
+      console.error("Error fetching current track:", error);
+    }
+  }, [currentTrack, isPlaying]);
+
+  const fetchCurrentTrack = useCallback(async () => {
+    try {
+      const res = await fetch(
+        "https://api.spotify.com/v1/me/player/currently-playing",
+        {
+          headers: {
+            Authorization: `Bearer ${session?.accessToken}`,
+          },
+        }
+      );
+
+      if (res.status === 204) {
+        setCurrentTrack(null);
+        return;
+      }
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch current track");
+      }
+
+      const data = await res.json();
+
+      if (data?.currently_playing_type === "track") {
+        const track = {
+          name: data.item.name,
+          link: data.item.external_urls.spotify,
+          artists: data.item.artists.map((artist) => ({
+            name: artist.name,
+            link: artist.external_urls.spotify,
+          })),
+          album: {
+            name: data.item.album.name,
+            images: data.item.album.images,
+            link: data.item.album.external_urls.spotify,
+          },
+          isPlaying: data.is_playing,
+          progressMs: data.progress_ms,
+          durationMs: data.item.duration_ms,
+          explicit: data.item.explicit,
+          popularity: data.item.popularity,
+        };
+
+        setCurrentTrack(track);
+        setProgress(track.progressMs);
+        setIsPlaying(track.isPlaying);
+      }
+    } catch (error) {
+      console.error("Error fetching current track:", error);
+    }
+  }, [session]);
+
+  const fetchCurrentPalette = useCallback(async () => {
+    if (!currentTrack?.album?.images[2]?.url) return;
+    document.title = `${currentTrack.name} - Spotify Web Player`;
+    try {
+      const colors = await extractColors(currentTrack.album.images[2].url, {
+        crossOrigin: "Anonymous",
+        // You can add more options here as needed
+      });
+      setCurrentPalette(colors);
+    } catch (error) {
+      console.error("Error extracting colors:", error);
+      setCurrentPalette(null);
+    }
+  }, [currentTrack?.album?.images[2]?.url]);
+
+  useEffect(() => {
+    if (session?.accessToken) {
+      fetchCurrentTrack();
+      const interval = setInterval(fetchCurrentTrack, 5000);
+      return () => clearInterval(interval);
+    }
+  }, [fetchCurrentTrack]);
+
+  useEffect(() => {
+    fetchCurrentPalette();
+  }, [fetchCurrentPalette]);
+
+  useEffect(() => {
+    if (!isPlaying || !currentTrack) return;
+
+    const progressInterval = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress < currentTrack.durationMs) {
+          return prevProgress + 1000;
+        }
+        return prevProgress;
+      });
+    }, 1000);
+
+    return () => clearInterval(progressInterval);
+  }, [currentTrack, isPlaying]);
+
+  const progressPercentage = useMemo(
+    () => (progress / (currentTrack?.durationMs || 1)) * 100,
+    [progress, currentTrack]
+  );
+
+  const value = useMemo(
+    () => ({
+      currentTrack,
+      isPlaying,
+      progress,
+      currentPalette,
+      progressPercentage,
+      togglePlay,
+    }),
+    [
+      currentTrack,
+      isPlaying,
+      progress,
+      currentPalette,
+      progressPercentage,
+      togglePlay,
+    ]
+  );
+
+  return (
+    <SpotifyContext.Provider value={value}>{children}</SpotifyContext.Provider>
+  );
+};
+
+export const useSpotify = () => {
+  const context = useContext(SpotifyContext);
+  if (context === undefined) {
+    throw new Error("useSpotify must be used within a SpotifyProvider");
+  }
+  return context;
+};
