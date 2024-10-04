4b79c60f7e9d74d73002d81caffcd04d5b7c9d6c
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-04 16:15:43 +0200

    add session check

---
+++ middleware.js
@@ -0,0 +1,12 @@
+import { auth } from "@/lib/auth";
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/api/auth/signin") {
+    const newUrl = new URL("/login", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
