a288c0a95574af3c98d27f3c67453d0485d7f7c9
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-04 16:17:11 +0200

    add tooltip

---
+++ components/ui/tooltip.js
@@ -0,0 +1,28 @@
+"use client";
+
+import { forwardRef } from "react";
+import { Provider, Root, Trigger, Content } from "@radix-ui/react-tooltip";
+
+const TooltipProvider = Provider;
+
+const Tooltip = Root;
+
+const TooltipTrigger = Trigger;
+
+const TooltipContent = forwardRef((props, ref) => {
+  const { className, sideOffset = 4, ...rest } = props;
+  return (
+    <Content
+      ref={ref}
+      sideOffset={sideOffset}
+      className={
+        "z-50 overflow-hidden rounded-xl border border-zinc-500/25 bg-zinc-900/25 px-3 py-1.5 text-sm m-1 text-zinc-200 shadow-md backdrop-blur-xl " +
+        className
+      }
+      {...rest}
+    />
+  );
+});
+TooltipContent.displayName = Content.displayName;
+
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
