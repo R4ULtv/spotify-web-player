29c5c1ed53481a36114793d1a2ff0ecb5d98cb6d
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-04 16:23:47 +0200

    fix link

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ export const config = {
 
 export default auth((req) => {
   if (!req.auth && req.nextUrl.pathname !== "/api/auth/signin") {
-    const newUrl = new URL("/login", req.nextUrl.origin);
+    const newUrl = new URL("/api/auth/signin", req.nextUrl.origin);
     return Response.redirect(newUrl);
   }
 });
