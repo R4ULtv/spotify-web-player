f80e0aa8b41ffe919d5819763fc17f7bd6e4c829
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-04 18:26:13 +0200

    moved to home page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,19 @@
-export default function Home() {
+import { SpotifyProvider } from "@/components/providers/SpotifyProvider";
+import RandomCircle from "@/components/ui/background";
+import SpotifyPlayer from "@/components/ui/SpotifyPlayer";
+import Navigation from "@/components/ui/Navigation";
+import { auth } from "@/lib/auth";
+
+export default async function Home() {
+  const session = await auth();
+
   return (
-    <></>
+    <SpotifyProvider session={session}>
+      <main className="relative w-full h-screen z-10">
+        <Navigation session={session} />
+        <SpotifyPlayer />
+      </main>
+      <RandomCircle />
+    </SpotifyProvider>
   );
 }
