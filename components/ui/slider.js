7c170344e07910d12c4f9ee32c9460c8f1172375
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-09 22:44:52 +0200

    add cursor

--- components/ui/slider.js
+++ components/ui/slider.js
@@ -9,10 +9,10 @@ const Slider = React.forwardRef(({ className, ...props }, ref) => (
     className={`relative flex w-full touch-none select-none items-center ${className}`}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-zinc-500/25">
+    <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-zinc-500/25 cursor-pointer">
       <SliderPrimitive.Range className="absolute h-full bg-zinc-200" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block size-2.5 rounded-full border-2 border-primary bg-zinc-200 transition-colors outline-none disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb className="block size-2.5 rounded-full border-2 border-primary bg-zinc-200 transition-colors cursor-pointer outline-none disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
