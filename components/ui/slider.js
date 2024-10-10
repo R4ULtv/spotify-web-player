738694c5302b6bbe14600e78fc00f42800315df4
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-10 20:41:28 +0200

    add tvMode

--- components/ui/slider.js
+++ components/ui/slider.js
@@ -3,16 +3,25 @@
 import * as React from "react";
 import * as SliderPrimitive from "@radix-ui/react-slider";
 
-const Slider = React.forwardRef(({ className, ...props }, ref) => (
+const Slider = React.forwardRef(({ className, size, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
     className={`relative flex w-full touch-none select-none items-center ${className}`}
     {...props}
   >
-    <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-zinc-500/25 cursor-pointer">
+    <SliderPrimitive.Track
+      className="relative w-full grow overflow-hidden rounded-full bg-zinc-500/25 cursor-pointer"
+      style={{ height: size ? `${size}px` : "4px" }}
+    >
       <SliderPrimitive.Range className="absolute h-full bg-zinc-200" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block size-2.5 rounded-full border-2 border-primary bg-zinc-200 transition-colors cursor-pointer outline-none disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb
+      className="block rounded-full bg-zinc-200 cursor-pointer outline-none disabled:pointer-events-none disabled:opacity-50"
+      style={{
+        height: size ? `${size * 2.5}px` : "10px",
+        width: size ? `${size * 2.5}px` : "10px",
+      }}
+    />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
