a9896266dd672d945f6845aa33bffb36bcddaa0f
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-11 19:07:50 +0200

    load only if theres a palette

--- components/ui/background.js
+++ components/ui/background.js
@@ -45,6 +45,7 @@ export default function RandomCircle() {
   }, [otherColors, isMobile]);
 
   useEffect(() => {
+    if (!currentPalette?.length) return;
     setCircles(generateCircles());
   }, [generateCircles]);
 
