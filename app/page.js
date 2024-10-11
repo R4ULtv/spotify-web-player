3ed77259f25b3adb3a8f827a3681edfe497cfecc
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-11 10:29:13 +0200

    add media provider

--- app/page.js
+++ app/page.js
@@ -1,20 +1,26 @@
 import { SessionProvider } from "next-auth/react";
 
 import { SpotifyProvider } from "@/components/providers/SpotifyProvider";
+import { MediaProvider } from "@/components/providers/MediaProvider";
 import RandomCircle from "@/components/ui/background";
 import SpotifyPlayer from "@/components/ui/SpotifyPlayer";
 import Navigation from "@/components/ui/Navigation";
 import TracksDrawer from "@/components/ui/TracksDrawer";
+import RotateDeviceDrawer from "@/components/ui/RotateDeviceDrawer";
 
 export default function Home() {
   return (
     <SessionProvider refetchOnWindowFocus={false}>
       <SpotifyProvider>
-        <main className="relative w-svw h-svh z-10">
-          <Navigation />
-          <SpotifyPlayer />
-          <TracksDrawer />
-        </main>
+        <MediaProvider>
+          <main className="relative w-svw h-svh z-10">
+            <Navigation />
+            <SpotifyPlayer />
+
+            <TracksDrawer />
+            <RotateDeviceDrawer />
+          </main>
+        </MediaProvider>
         <RandomCircle />
       </SpotifyProvider>
     </SessionProvider>
---
+++ components/providers/MediaProvider.js
@@ -0,0 +1,65 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  useMemo,
+  useState,
+  useEffect,
+  useCallback,
+} from "react";
+
+// Create a context for Media-related data and functions
+const MediaContext = createContext();
+
+export const MediaProvider = ({ children }) => {
+  const [isPortrait, setIsPortrait] = useState(false);
+  const [isOpenTrackDrawer, setIsOpenTrackDrawer] = useState(false);
+  const [isOpenRotateDeviceDrawer, setIsOpenRotateDeviceDrawer] =
+    useState(false);
+
+  const checkOrientation = useCallback(() => {
+    setIsPortrait(window.innerHeight > window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    checkOrientation();
+    window.addEventListener("resize", checkOrientation);
+    return () => window.removeEventListener("resize", checkOrientation);
+  }, [checkOrientation]);
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === "p") {
+        setIsOpenTrackDrawer((prev) => !prev);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+    return () => window.removeEventListener("keydown", handleKeyPress);
+  }, []);
+
+  const value = useMemo(
+    () => ({
+      isOpenTrackDrawer,
+      isOpenRotateDeviceDrawer,
+      isPortrait,
+      setIsOpenTrackDrawer,
+      setIsOpenRotateDeviceDrawer,
+    }),
+    [isOpenTrackDrawer, isOpenRotateDeviceDrawer, isPortrait]
+  );
+
+  return (
+    <MediaContext.Provider value={value}>{children}</MediaContext.Provider>
+  );
+};
+
+// Custom hook to access Media context
+export const useMedia = () => {
+  const context = useContext(MediaContext);
+  if (!context) {
+    throw new Error("useMedia must be used within a MediaProvider");
+  }
+  return context;
+};
