1b6271e2c0a985fa0a4d987c292b28f48ca88e76
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-22 18:49:03 +0200

    update signin page

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,80 +1,45 @@
-import { Button } from "@headlessui/react";
 import { redirect } from "next/navigation";
-import { signIn, auth } from "@/lib/auth";
-import { SpotifyIcon } from "@/components/utils/icons";
+import { auth } from "@/lib/auth";
 
-export const runtime = "edge";
+import SignInButton from "@/components/ui/SignInButton";
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import FakeSpotifyPlayer from "@/components/ui/FakeSpotifyPlayer";
+import { StarsBackground } from "@/components/layout/stars-background";
+import { ShootingStars } from "@/components/layout/shooting-stars";
 
-export default async function Login({ searchParams }) {
-  const session = await auth();
-
-  if (session) {
-    redirect("/");
-  }
-
-  const handleLogin = async () => {
-    "use server";
-    await signIn("spotify", { redirectTo: "/" });
-  };
-
+export default function Login() {
   return (
-    <main className="relative">
-      <div className="flex flex-col items-center justify-center gap-8 relative z-10 h-svh w-svw">
-        <div>
-          <h1 className="text-5xl sm:text-7xl font-black text-center relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-zinc-200 to-zinc-500">
-            Spotify Web Player
-          </h1>
+    <main>
+      <div className="relative min-h-full md:h-full">
+        <div className="max-w-7xl mx-auto flex flex-col items-center justify-between md:justify-center w-full h-full px-3 py-8 md:px-8 md:py-40 z-10 relative">
+          <div className="max-w-3xl">
+            <h1 className="text-5xl sm:text-7xl text-zinc-100 font-black text-balance text-center">
+              Listen to your Music Anytime, Anywhere
+            </h1>
 
-          <form
-            action={handleLogin}
-            className="mt-2 sm:mt-6 flex flex-col items-center justify-center"
-          >
-            {searchParams.error && (
-              <p className="text-sm first-letter:text-zinc-400 mb-1">
-                Something went wrong.
-              </p>
-            )}
+            <p className="mt-4 md:mt-8 text-balance mx-auto text-zinc-300 text-base font-medium text-center">
+              Connect your Spotify account and experience your music like never
+              before, with stunning visuals that dance to your favorite tunes.
+            </p>
 
-            <Button
-              type="submit"
-              className="bg-zinc-800/50 data-[hover]:bg-zinc-800/75 backdrop-blur-xl text-zinc-100 px-3 py-1.5 rounded-md flex gap-2 items-center group"
-            >
-              <SpotifyIcon className="size-5 group-data-[hover]:scale-110 group-data-[focus]:scale-110 duration-75" />
-              <span className="font-medium select-none">
-                Sign in with Spotify
-              </span>
-            </Button>
-          </form>
+            <div className="mt-4 md:mt-8 flex items-center justify-center flex-col md:flex-row gap-3">
+              <SignInButton />
+              <a
+                href="#more"
+                className="bg-zinc-100 text-spotify-black px-5 py-2 rounded-full flex gap-1.5 items-center group"
+              >
+                <GlobeAltIcon className="size-5 group-hover:scale-110 group-hover:rotate-12 group-focus:scale-110 group-focus:rotate-12 duration-75 ease-out" />
+                <span className="font-semibold">Discorver More</span>
+              </a>
+            </div>
+          </div>
+          <div className="mt-8 md:mt-16 w-full">
+            <FakeSpotifyPlayer />
+          </div>
         </div>
-        <div className="sm:absolute bottom-10">
-          <p className="text-sm text-zinc-400 max-w-xl text-center text-balance mt-3">
-            This website is <span className="font-semibold">open-source</span>.
-            You can find the code on{" "}
-            <a
-              href="https://github.com/r4ultv/spotify-web-player"
-              className="font-semibold"
-            >
-              GitHub
-            </a>
-            . If you need more information you can check out my{" "}
-            <a
-              href="https://www.raulcarini.dev/blog/spotify-web-player"
-              className="font-semibold"
-            >
-              blog post
-            </a>
-            .
-          </p>
-
-          <p className="text-sm text-zinc-400 max-w-xl text-center text-balance mt-3">
-            Built for You by{" "}
-            <a href="https://www.raulcarini.dev" className="font-semibold">
-              Raul Carini
-            </a>
-          </p>
-        </div>
+        <ShootingStars />
+        <StarsBackground />
       </div>
-      <div className="absolute inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMDAiIGhlaWdodD0iMzAwIj4KICA8ZmlsdGVyIGlkPSJub2lzZSIgeD0iMCIgeT0iMCIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSI+CiAgICA8ZmVUdXJidWxlbmNlIHR5cGU9ImZyYWN0YWxOb2lzZSIgYmFzZUZyZXF1ZW5jeT0iMC42NSIgc3RpdGNoVGlsZXM9InN0aXRjaCIgbnVtT2N0YXZlcz0iMyIgc2VlZD0iMiIgcmVzdWx0PSJub2lzZSI+PC9mZVR1cmJ1bGVuY2U+CiAgICA8ZmVDb2xvck1hdHJpeCB0eXBlPSJzYXR1cmF0ZSIgdmFsdWVzPSIwIj48L2ZlQ29sb3JNYXRyaXg+CiAgPC9maWx0ZXI+CiAgPHJlY3Qgd2lkdGg9IjMwMCIgaGVpZ2h0PSIzMDAiIGZpbHRlcj0idXJsKCNub2lzZSkiIG9wYWNpdHk9IjAuNCI+PC9yZWN0Pgo8L3N2Zz4=')] opacity-50" />
     </main>
   );
 }
