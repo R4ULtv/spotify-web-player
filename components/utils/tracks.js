b8ceb5153d51333dcc979d70dd042d7a0ee2b395
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-22 18:47:59 +0200

    add example tracks

---
+++ components/utils/tracks.js
@@ -0,0 +1,390 @@
+export const exampleTracks = [
+  {
+    name: "Starboy",
+    link: "https://open.spotify.com/track/7MXVkk9YMctZqd1Srtv4MB",
+    artists: [
+      {
+        name: "The Weeknd",
+        link: "https://open.spotify.com/artist/1Xyo4u8uXC1ZmMpatF05PJ",
+      },
+      {
+        name: "Daft Punk",
+        link: "https://open.spotify.com/artist/4tZwfgrHOc3mvqYlEYSvVi",
+      },
+    ],
+    album: {
+      name: "Starboy",
+      link: "https://open.spotify.com/album/2ODvWsOgouMbaA5xf0RkJe",
+      image: "https://i.scdn.co/image/ab67616d00001e024718e2b124f79258be7bc452",
+    },
+    durationMs: 230453,
+    explicit: true,
+    currentPalette: ["#efc703", "#e60837", "#170304"],
+  },
+  {
+    name: "Plug Walk",
+    link: "https://open.spotify.com/track/1ZAyjvIk9YiD76yYy0TEG6",
+    artists: [
+      {
+        name: "Rich The Kid",
+        link: "https://open.spotify.com/artist/1pPmIToKXyGdsCF6LmqLmI",
+      },
+    ],
+    album: {
+      name: "The World Is Yours",
+      link: "https://open.spotify.com/album/1Hno6CtKbaFNAEPjRHDrc6",
+      image: "https://i.scdn.co/image/ab67616d00001e0264353426de6dfe0206f6d1d3",
+    },
+    durationMs: 175229,
+    explicit: true,
+    currentPalette: ["#ee9ab1", "#8e6762", "#74bdce"],
+  },
+  {
+    name: "No Role Modelz",
+    link: "https://open.spotify.com/track/68Dni7IE4VyPkTOH9mRWHr",
+    artists: [
+      {
+        name: "J. Cole",
+        link: "https://open.spotify.com/artist/6l3HvQ5sa6mXTsMTB19rO5",
+      },
+    ],
+    album: {
+      name: "2014 Forest Hills Drive",
+      link: "https://open.spotify.com/album/0UMMIkurRUmkruZ3KGBLtG",
+      image: "https://i.scdn.co/image/ab67616d00001e02c6e0948bbb0681ff29cdbae8",
+    },
+    durationMs: 292799,
+    explicit: true,
+    currentPalette: ["#90b1d2", "#506270", "#232522"],
+  },
+  {
+    name: "Love Me More",
+    link: "https://open.spotify.com/track/3sDyGrpProusVfcccFLAHq",
+    artists: [
+      {
+        name: "Trippie Redd",
+        link: "https://open.spotify.com/artist/6Xgp2XMz1fhVYe7i6yNAax",
+      },
+    ],
+    album: {
+      name: "A Love Letter To You 4",
+      link: "https://open.spotify.com/album/5nDqjtvRwDYElIflvoNDdE",
+      image: "https://i.scdn.co/image/ab67616d00001e02338fabbb1729a74d655a6a85",
+    },
+    durationMs: 143712,
+    explicit: true,
+    currentPalette: ["#c6926b", "#783b38", "#290a08"],
+  },
+  {
+    name: "Anxiety",
+    link: "https://open.spotify.com/track/2LcYVpLyvXyKmIoOkOItzi",
+    artists: [
+      {
+        name: "lil-poop",
+        link: "https://open.spotify.com/artist/21DbatkhkYpxIpLfQ4LeUo",
+      },
+    ],
+    album: {
+      name: "Anxiety",
+      link: "https://open.spotify.com/album/3lmoFhxUN01DqyNdprnMlS",
+      image: "https://i.scdn.co/image/ab67616d00001e02408ebfefefa9af0c18c2dc23",
+    },
+    durationMs: 138228,
+    explicit: false,
+    currentPalette: ["#916fdd", "#4b2da7", "#030043"],
+  },
+  {
+    name: "Lucid Dreams",
+    link: "https://open.spotify.com/track/285pBltuF7vW8TeWk8hdRR",
+    artists: [
+      {
+        name: "Juice WRLD",
+        link: "https://open.spotify.com/artist/4MCBfE4596Uoi2O4DtmEMz",
+      },
+    ],
+    album: {
+      name: "Goodbye & Good Riddance",
+      link: "https://open.spotify.com/album/6tkjU4Umpo79wwkgPMV3nZ",
+      image: "https://i.scdn.co/image/ab67616d00001e02f7db43292a6a99b21b51d5b4",
+    },
+    durationMs: 239835,
+    explicit: true,
+    currentPalette: ["#1094d4", "#f4dd69", "#201a1a"],
+  },
+  {
+    name: "The Way Life Goes (feat. Oh Wonder)",
+    link: "https://open.spotify.com/track/2eAZfqOm4EnOF9VvN50Tyc",
+    artists: [
+      {
+        name: "Lil Uzi Vert",
+        link: "https://open.spotify.com/artist/4O15NlyKLIASxsJ0PrXPfz",
+      },
+      {
+        name: "Oh Wonder",
+        link: "https://open.spotify.com/artist/5cIc3SBFuBLVxJz58W2tU9",
+      },
+    ],
+    album: {
+      name: "Luv Is Rage 2",
+      link: "https://open.spotify.com/album/733e1ZfktLSwj96X5rsMeE",
+      image: "https://i.scdn.co/image/ab67616d00001e0241b41e6f7924e530a9b00f9c",
+    },
+    durationMs: 221746,
+    explicit: true,
+    currentPalette: ["#cad5e9", "#939393", "#121212"],
+  },
+  {
+    name: "Nightcall",
+    link: "https://open.spotify.com/track/0U0ldCRmgCqhVvD6ksG63j",
+    artists: [
+      {
+        name: "Kavinsky",
+        link: "https://open.spotify.com/artist/0UF7XLthtbSF2Eur7559oV",
+      },
+    ],
+    album: {
+      name: "Nightcall",
+      link: "https://open.spotify.com/album/07nBld9enf1PyRysZAVSqJ",
+      image: "https://i.scdn.co/image/ab67616d00001e02d6d8c2eaa1f9031b62f7a3f7",
+    },
+    durationMs: 258413,
+    explicit: false,
+    currentPalette: ["#d71925", "#133d6f", "#826d7c"],
+  },
+  {
+    name: "ABOUT YOU (with NAV)",
+    link: "https://open.spotify.com/track/098a0ODkRj8IUbEGVZMsyN",
+    artists: [
+      {
+        name: "Lil Tecca",
+        link: "https://open.spotify.com/artist/4Ga1P7PMIsmqEZqhYZQgDo",
+      },
+      {
+        name: "NAV",
+        link: "https://open.spotify.com/artist/7rkW85dBwwrJtlHRDkJDAC",
+      },
+    ],
+    album: {
+      name: "We Love You Tecca 2",
+      link: "https://open.spotify.com/album/7nTvm6EgmFWPLMLxj6aksS",
+      image: "https://i.scdn.co/image/ab67616d00001e02f156ced88fa6920e86ddf11f",
+    },
+    durationMs: 204137,
+    explicit: true,
+    currentPalette: ["#a05329", "#fc7885", "#2075a3"],
+  },
+  {
+    name: "ALONE (feat. Trippie Redd)",
+    link: "https://open.spotify.com/track/3nifv7CIkBQTOKzATNEGNK",
+    artists: [
+      {
+        name: "$NOT",
+        link: "https://open.spotify.com/artist/5IbEL2xjRtKsunfmsahLuO",
+      },
+      {
+        name: "Trippie Redd",
+        link: "https://open.spotify.com/artist/6Xgp2XMz1fhVYe7i6yNAax",
+      },
+    ],
+    album: {
+      name: "Ethereal",
+      link: "https://open.spotify.com/album/5C1X3siQg4Q3wLWorK5i0z",
+      image: "https://i.scdn.co/image/ab67616d00001e02ee72c43250d377ee9a3de52e",
+    },
+    durationMs: 136600,
+    explicit: true,
+    currentPalette: ["#3d4584", "#4c84e3", "#121318"],
+  },
+  {
+    name: "Smalltown Boy",
+    link: "https://open.spotify.com/track/5vmRQ3zELMLUQPo2FLQ76x",
+    artists: [
+      {
+        name: "Bronski Beat",
+        link: "https://open.spotify.com/artist/2wpWOzQE5TpA0dVnh5YD08",
+      },
+    ],
+    album: {
+      name: "The Age of Consent",
+      link: "https://open.spotify.com/album/6OMYQUITdN6wBaWfEtgooI",
+      image: "https://i.scdn.co/image/ab67616d00001e026affdd29d6ee84a298746ef7",
+    },
+    durationMs: 302413,
+    explicit: false,
+    currentPalette: ["#edea29", "#a6350d", "#e4478a"],
+  },
+  {
+    name: "Congratulations",
+    link: "https://open.spotify.com/track/3a1lNhkSLSkpJE4MSHpDu9",
+    artists: [
+      {
+        name: "Post Malone",
+        link: "https://open.spotify.com/artist/246dkjvS1zLTtiykXe5h60",
+      },
+      {
+        name: "Quavo",
+        link: "https://open.spotify.com/artist/0VRj0yCOv2FXJNP47XQnx5",
+      },
+    ],
+    album: {
+      name: "Stoney (Deluxe)",
+      link: "https://open.spotify.com/album/5s0rmjP8XOPhP6HhqOhuyC",
+      image: "https://i.scdn.co/image/ab67616d00001e0255404f712deb84d0650a4b41",
+    },
+    durationMs: 220293,
+    explicit: true,
+    currentPalette: ["#f08751", "#616264", "#122521"],
+  },
+  {
+    name: "Hot (feat. Gunna)",
+    link: "https://open.spotify.com/track/5Z8HZM6iQMhhqyPcCGY5g9",
+    artists: [
+      {
+        name: "Young Thug",
+        link: "https://open.spotify.com/artist/50co4Is1HCEo8bhOyUWKpn",
+      },
+      {
+        name: "Gunna",
+        link: "https://open.spotify.com/artist/2hlmm7s2ICUX0LVIhVFlZQ",
+      },
+    ],
+    album: {
+      name: "So Much Fun",
+      link: "https://open.spotify.com/album/1bnHPO4dKK7IjvgrtVBcQh",
+      image: "https://i.scdn.co/image/ab67616d00001e026fcd1b6e205d0d19d9efa0cc",
+    },
+    durationMs: 193026,
+    explicit: true,
+    currentPalette: ["#a0a294", "#4c690c", "#121212"],
+  },
+  {
+    name: "SICKO MODE",
+    link: "https://open.spotify.com/track/2xLMifQCjDGFmkHkpNLD9h",
+    artists: [
+      {
+        name: "Travis Scott",
+        link: "https://open.spotify.com/artist/0Y5tJX1MQlPlqiwlOH1tJY",
+      },
+    ],
+    album: {
+      name: "ASTROWORLD",
+      link: "https://open.spotify.com/album/41GuZcammIkupMPKH2OJ6I",
+      image: "https://i.scdn.co/image/ab67616d00001e02072e9faef2ef7b6db63834a3",
+    },
+    durationMs: 312820,
+    explicit: true,
+    currentPalette: ["#fb0000", "#f7c44f", "#2064a1"],
+  },
+  {
+    name: "Tremor",
+    link: "https://open.spotify.com/track/2VlqJLH1ILRs8OqpQuoJ4O",
+    artists: [
+      {
+        name: "lil-poop",
+        link: "https://open.spotify.com/artist/21DbatkhkYpxIpLfQ4LeUo",
+      },
+    ],
+    album: {
+      name: "Rage Mix (The Art of Murder)",
+      link: "https://open.spotify.com/album/0N5CoavdUuxWWtmpaBCJPq",
+      image: "https://i.scdn.co/image/ab67616d00001e0217fe8f4f754ed9ab068873e2",
+    },
+    durationMs: 175647,
+    explicit: false,
+    currentPalette: ["#f33233", "#111717", "#121212"],
+  },
+  {
+    name: "Bank Account",
+    link: "https://open.spotify.com/track/5eqK0tbzUPo2SoeZsov04s",
+    artists: [
+      {
+        name: "21 Savage",
+        link: "https://open.spotify.com/artist/1URnnhqYAYcrqrcwql10ft",
+      },
+    ],
+    album: {
+      name: "Issa Album",
+      link: "https://open.spotify.com/album/4skCiJhVVSKrDOBtoFbsxU",
+      image: "https://i.scdn.co/image/ab67616d00001e022719343298d0e4f1790e14cd",
+    },
+    durationMs: 220306,
+    explicit: true,
+    currentPalette: ["#c9533b", "#f29794", "#332620"],
+  },
+  {
+    name: "Trap Queen",
+    link: "https://open.spotify.com/track/0TzxcB6dK46vgXZT2P8qeR",
+    artists: [
+      {
+        name: "Fetty Wap",
+        link: "https://open.spotify.com/artist/6PXS4YHDkKvl1wkIl4V8DL",
+      },
+    ],
+    album: {
+      name: "Fetty Wap",
+      link: "https://open.spotify.com/album/1wCr7zhB4lMa2vZVhu4ty6",
+      image: "https://i.scdn.co/image/ab67616d00001e0202928b251e41844f5186920e",
+    },
+    durationMs: 222128,
+    explicit: true,
+    currentPalette: ["#b49579", "#e1d1c0", "#111717"],
+  },
+  {
+    name: "Ocean Drive",
+    link: "https://open.spotify.com/track/02gySWUFk5JT4gMguGAhdO",
+    artists: [
+      {
+        name: "Miami Nights 1984",
+        link: "https://open.spotify.com/artist/18iQQOuyGlHunPVzmoLY20",
+      },
+    ],
+    album: {
+      name: "Turbulence",
+      link: "https://open.spotify.com/album/1sjg3emGXcOFStmqIUvFeU",
+      image: "https://i.scdn.co/image/ab67616d00001e02674dce9398ee42d991cdf0cd",
+    },
+    durationMs: 258461,
+    explicit: false,
+    currentPalette: ["#f3ff1c", "#da2fa2", "#a5a318"],
+  },
+  {
+    name: "Bad and Boujee (feat. Lil Uzi Vert)",
+    link: "https://open.spotify.com/track/0M9ydKzuF3oZTfYYPfaGX1",
+    artists: [
+      {
+        name: "Migos",
+        link: "https://open.spotify.com/artist/6oMuImdp5ZcFhWP0ESe6mG",
+      },
+      {
+        name: "Lil Uzi Vert",
+        link: "https://open.spotify.com/artist/4O15NlyKLIASxsJ0PrXPfz",
+      },
+    ],
+    album: {
+      name: "Culture",
+      link: "https://open.spotify.com/album/4JTOxuvM2jcSqAvEZtZsOO",
+      image: "https://i.scdn.co/image/ab67616d00001e026275aeac316378b0dd4f31fd",
+    },
+    durationMs: 343150,
+    explicit: true,
+    currentPalette: ["#d47326", "#c1c2c7", "#0b0b0b"],
+  },
+  {
+    name: "2055",
+    link: "https://open.spotify.com/track/4XvcHTUfIlWfyJTRG0aqlo",
+    artists: [
+      {
+        name: "Sleepy Hallow",
+        link: "https://open.spotify.com/artist/6EPlBSH2RSiettczlz7ihV",
+      },
+    ],
+    album: {
+      name: "Still Sleep?",
+      link: "https://open.spotify.com/album/38nZLvJx8A9p4YYpTG5iFP",
+      image: "https://i.scdn.co/image/ab67616d00001e0278376f650afd63698b82e4b1",
+    },
+    durationMs: 123986,
+    explicit: true,
+    currentPalette: ["#b805eb", "#5183b8", "#98b4ff"],
+  },
+];
