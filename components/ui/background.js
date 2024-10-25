3374d682affeefe255692faa9a98993c941749df
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-10-25 18:54:55 +0200

    removed transition from background

--- components/ui/background.js
+++ components/ui/background.js
@@ -1,55 +1,49 @@
 "use client";
 
 import { useMemo } from "react";
-import { Transition } from "@headlessui/react";
 import { ShaderGradient, ShaderGradientCanvas } from "shadergradient";
 import { useSpotify } from "@/components/providers/SpotifyProvider";
 import { calculateDeltaE } from "@/components/utils/hooks";
 
 export const GradientBackground = ({ className, colors }) => {
+  if (colors.length === 0) return null;
   return (
-    <Transition show={!!colors} appear>
-      <div className={className}>
-        <div className="absolute z-[2] inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMDAiIGhlaWdodD0iMzAwIj4KICA8ZmlsdGVyIGlkPSJub2lzZSIgeD0iMCIgeT0iMCIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSI+CiAgICA8ZmVUdXJidWxlbmNlIHR5cGU9ImZyYWN0YWxOb2lzZSIgYmFzZUZyZXF1ZW5jeT0iMC42NSIgc3RpdGNoVGlsZXM9InN0aXRjaCIgbnVtT2N0YXZlcz0iMyIgc2VlZD0iMiIgcmVzdWx0PSJub2lzZSI+PC9mZVR1cmJ1bGVuY2U+CiAgICA8ZmVDb2xvck1hdHJpeCB0eXBlPSJzYXR1cmF0ZSIgdmFsdWVzPSIwIj48L2ZlQ29sb3JNYXRyaXg+CiAgPC9maWx0ZXI+CiAgPHJlY3Qgd2lkdGg9IjMwMCIgaGVpZ2h0PSIzMDAiIGZpbHRlcj0idXJsKCNub2lzZSkiIG9wYWNpdHk9IjAuNCI+PC9yZWN0Pgo8L3N2Zz4=')] opacity-50" />
-        <ShaderGradientCanvas>
-          <ShaderGradient
-            animate="on"
-            brightness={0.7}
-            cAzimuthAngle={180}
-            cDistance={2.8}
-            cPolarAngle={115}
-            cameraZoom={9.1}
-            color1={colors[0]}
-            color2={colors[1]}
-            color3={colors[2]}
-            frameRate={60}
-            grain="off"
-            lightType="3d"
-            positionX={0}
-            positionY={0}
-            positionZ={0}
-            range="disabled"
-            rangeEnd={40}
-            rangeStart={0}
-            reflection={0.1}
-            rotationX={0}
-            rotationY={0}
-            rotationZ={235}
-            shader="defaults"
-            type="waterPlane"
-            uAmplitude={0}
-            uDensity={1.1}
-            uFrequency={0}
-            uSpeed={0.1}
-            uStrength={2.4}
-            uTime={8}
-            wireframe={false}
-            zoomOut={false}
-            enableTransition={false}
-          />
-        </ShaderGradientCanvas>
-      </div>
-    </Transition>
+    <div className={className}>
+      <div className="absolute z-[2] inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMDAiIGhlaWdodD0iMzAwIj4KICA8ZmlsdGVyIGlkPSJub2lzZSIgeD0iMCIgeT0iMCIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSI+CiAgICA8ZmVUdXJidWxlbmNlIHR5cGU9ImZyYWN0YWxOb2lzZSIgYmFzZUZyZXF1ZW5jeT0iMC42NSIgc3RpdGNoVGlsZXM9InN0aXRjaCIgbnVtT2N0YXZlcz0iMyIgc2VlZD0iMiIgcmVzdWx0PSJub2lzZSI+PC9mZVR1cmJ1bGVuY2U+CiAgICA8ZmVDb2xvck1hdHJpeCB0eXBlPSJzYXR1cmF0ZSIgdmFsdWVzPSIwIj48L2ZlQ29sb3JNYXRyaXg+CiAgPC9maWx0ZXI+CiAgPHJlY3Qgd2lkdGg9IjMwMCIgaGVpZ2h0PSIzMDAiIGZpbHRlcj0idXJsKCNub2lzZSkiIG9wYWNpdHk9IjAuNCI+PC9yZWN0Pgo8L3N2Zz4=')] opacity-50" />
+      <ShaderGradientCanvas>
+        <ShaderGradient
+          animate="on"
+          brightness={0.7}
+          cAzimuthAngle={180}
+          cDistance={4}
+          cPolarAngle={115}
+          color1={colors[0]}
+          color2={colors[1]}
+          color3={colors[2]}
+          grain="off"
+          lightType="3d"
+          positionX={0}
+          positionY={0}
+          positionZ={0}
+          range="disabled"
+          reflection={0.1}
+          rotationX={0}
+          rotationY={0}
+          rotationZ={235}
+          shader="defaults"
+          type="waterPlane"
+          uAmplitude={0}
+          uDensity={1.1}
+          uFrequency={0}
+          uSpeed={0.1}
+          uStrength={2.4}
+          uTime={8}
+          wireframe={false}
+          zoomOut={false}
+          enableTransition={false}
+        />
+      </ShaderGradientCanvas>
+    </div>
   );
 };
 
